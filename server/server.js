989ca01f5363c70b3d5ceeb4c7d67ff6deddd022
Anna Falvello <anna.falvello@gmail.com>
2021-11-03 19:40:19 -0400

    After Restoring to Pre-Mongo

--- server/server.js
+++ server/server.js
@@ -1,38 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { default: DataBox } = require('../client/components/DataBox');
-//const db = require('../model/userFunctions')
 
-// If "fake" database
 const users = require((path.join(__dirname, '../model/userDatabase')))
 
 
 app.get('/verify', (req, res) => {
   const username = req.query.username;
   const password = req.query.password;
-  // // Fake Database
   if (users[username].password == password) {
     const userSessionData = users[username].data;
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     return res.status(200).json(userSessionData);
-  }
+    }
   else {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header("Access-Control-Allow-Origin", "*")
     return res.status(404).send('Username or Password Incorrect');
   }
-
-  // JSON Database
-  // const currentUser = db.find(username)
-  // if (currentUser.password == password) {
-  //   const userSessionData = currentUser.data;
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   return res.status(200).json(userSessionData);
-  // }
-  // else {
-  //   res.header('Access-Control-Allow-Origin', '*')
-  //   return res.status(404).send('Username or Password Incorrect');
-  // }
 });
 
 if (process.env.NODE_ENV === 'production') {
